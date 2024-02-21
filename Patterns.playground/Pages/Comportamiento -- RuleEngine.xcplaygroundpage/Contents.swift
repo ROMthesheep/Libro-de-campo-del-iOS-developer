4dfdda9f532b89d3ea7a18107afe8773dd3f22a8
Rom The Sheep <romthesheep@gmail.com>
2024-02-21 08:08:21 +0100

    Added proxy and rule engine

---
+++ Patterns.playground/Pages/Comportamiento -- RuleEngine.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/**
+ Utiliza ek patrón Rule Engine cuando necesites de forma masiva hacer una accionncuando se den unas condiciones. Este diseño permite definir tareas complejas fuera de la logica principal.
+ Es una herramienta poderosa para encapsular, aislar y proteger procesos ligados al tipo de input que se da sin exponerlo al dueño del mismo.
+ El patron cuenta con tres partes diferenciadas, Las reglas, el motor y los datos.
+La dinammica principal es, el motor mediante unas reglas ejecuta unna serie de procesos contra una entidad y devuelve el producto de ese  proceso.
+ 
+ Los principales pros de este acercamiento son:
+ Alto grado de mantenimiento
+ Alto grado de flexibiidad
+ Alto grado de reusabilidad
+ Separacion clara de respponsabilidades
+ Centralizacion de tareas
+ */
+
+struct Rule {
+    let validate: (String) -> Bool
+    let replace: () -> String
+}
+
+extension Rule {
+    private static let wordsNotAllowed = ["insult1", "insult2", "insult3"]
+    static let removeInsults = Rule { subject in
+        wordsNotAllowed.contains(where: subject.contains)
+    } replace: {
+        // Do something to penalize the user
+        "Message deleted by auto moderator"
+    }}
+
+protocol RuleEngine {
+    func execute(with value: String) -> String
+}
+
+struct DemoRuleEngine: RuleEngine {
+    let rules: [Rule]
+    
+    func execute(with value: String) -> String {
+        var result = String()
+        rules.forEach { rule in
+            result = rule.validate(value) ? rule.replace() : value
+        }
+        return result
+    }
+}
+
+let messages = ["Hi! how's your day going?",
+                "Did you se that insult1 that was on the front door?",
+                "she was making me crazy",
+                "what an insult3"]
+let ruleEngine = DemoRuleEngine(rules: [Rule.removeInsults])
+
+messages.forEach { message in
+    print(ruleEngine.execute(with: message))
+}
+
+//: [Next](@next)
--- Patterns.playground/Pages/Estructurales -- Proxy.xcplaygroundpage/Contents.swift
+++ Patterns.playground/Pages/Estructurales -- Proxy.xcplaygroundpage/Contents.swift
@@ -2,6 +2,28 @@
 
 import Foundation
 
+/**
+ Inicialización diferida (proxy virtual). Es cuando tienes un objeto de servicio muy pesado que utiliza muchos recursos del sistema al estar siempre funcionando, aunque solo lo necesites de vez en cuando.
+    En lugar de crear el objeto cuando se lanza la aplicación, puedes retrasar la inicialización del objeto a un momento en que sea realmente necesario.
+
+Control de acceso (proxy de protección). Es cuando quieres que únicamente clientes específicos sean capaces de utilizar el objeto de servicio, por ejemplo, cuando tus objetos son partes fundamentales de un sistema operativo y los clientes son varias aplicaciones lanzadas (incluyendo maliciosas).
+    El proxy puede pasar la solicitud al objeto de servicio tan sólo si las credenciales del cliente cumplen ciertos criterios.
+
+Ejecución local de un servicio remoto (proxy remoto). Es cuando el objeto de servicio se ubica en un servidor remoto.
+    En este caso, el proxy pasa la solicitud del cliente por la red, gestionando todos los detalles desagradables de trabajar con la red.
+
+Solicitudes de registro (proxy de registro). Es cuando quieres mantener un historial de solicitudes al objeto de servicio.
+    El proxy puede registrar cada solicitud antes de pasarla al servicio.
+
+Resultados de solicitudes en caché (proxy de caché). Es cuando necesitas guardar en caché resultados de solicitudes de clientes y gestionar el ciclo de vida de ese caché, especialmente si los resultados son muchos.
+    El proxy puede implementar el caché para solicitudes recurrentes que siempre dan los mismos resultados. El proxy puede utilizar los parámetros de las solicitudes como claves de caché.
+
+Referencia inteligente. Es cuando debes ser capaz de desechar un objeto pesado una vez que no haya clientes que lo utilicen.
+    El proxy puede rastrear los clientes que obtuvieron una referencia del objeto de servicio o sus resultados. De vez en cuando, el proxy puede recorrer los clientes y comprobar si siguen activos. Si la lista del cliente se vacía, el proxy puede desechar el objeto de servicio y liberar los recursos subyacentes del sistema.
+    El proxy también puede rastrear si el cliente ha modificado el objeto de servicio. Después, los objetos sin cambios pueden ser reutilizados por otros clientes.
+
+ */
+
 protocol Sujeto {
     func accion()
 }
