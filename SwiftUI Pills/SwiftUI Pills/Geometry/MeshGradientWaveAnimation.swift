ff96fde439a609f917335563ef627e56f1d39327
Rom🐏 <romthesheep@gmail.com>
2024-06-12 11:20:27 +0200

    numbers tweaked

--- SwiftUI Pills/SwiftUI Pills/Geometry/MeshGradientWaveAnimation.swift
+++ SwiftUI Pills/SwiftUI Pills/Geometry/MeshGradientWaveAnimation.swift
@@ -25,15 +25,15 @@ struct MeshGradientWaveAnimation: View {
                     // create wave on center
                     [ // 0,1
                         rangedWave(-1...(-0.5), 0.125, increment),
-                        rangedWave (0.3...0.6, 1, increment)
+                        rangedWave (0.3...0.6, 0.9, increment)
                     ],
                     [ // 1,1
-                        rangedWave(0.2...1, 0.1, increment),
+                        rangedWave(0.2...0.7, 0.1, increment),
                         rangedWave (0.2...0.8, 0.3, increment)
                     ],
                     [ // 2,1
                         rangedWave(1.0...1.5, 0.01, increment),
-                        rangedWave (0.5...1, 0.8, increment)
+                        rangedWave (0.4...0.8, 0.6, increment)
                     ],
                     // clamp foot
                     [0, 1],
@@ -41,14 +41,21 @@ struct MeshGradientWaveAnimation: View {
                     [1, 1],
                 ],
                 colors: [
-                    .white, .white, .white,
+                    .mint, .mint, .white,
                     .blue, .cyan, .indigo,
-                    .white, .white, .mint
+                    .white, .mint, .mint
                 ]
             ).ignoresSafeArea()
+            VStack {
+                Text("raw value: \(String(format: "%.3f", increment))")
+                    .font(.headline)
+                Text("oscilating value: \(String(format: "%.3f", rangedWave (-1...1, 1, increment)))")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }.foregroundColor(.white)
         }
         .onAppear {
-            timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { _ in
+            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
                 increment += 0.01
             })
         }
